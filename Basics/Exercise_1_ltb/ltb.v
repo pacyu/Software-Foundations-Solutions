9dacf8b2997007d40e295ef65b385ee59669fce2
darkchii <darkchii@qq.com>
2020-10-29 22:34:00 +0800

    Update

--- Basics/Exercise_1_ltb/ltb.v
+++ Basics/Exercise_1_ltb/ltb.v
@@ -27,8 +27,8 @@ Definition ltb (n m : nat) : bool :=
   end.
 Notation "x <? y" := (ltb x y) (at level 70) : nat_scope.
 Example test_ltb1: (ltb 2 2) = false.
-Proof. Qed.
+Proof. reflexivity. Qed.
 Example test_ltb2: (ltb 2 4) = true.
-(* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 Example test_ltb3: (ltb 4 2) = false.
-(* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
